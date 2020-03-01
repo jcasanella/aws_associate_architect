49be8145aa2b34241a473297b711f8c3b6950598
jordi <jordikiter@hotmail.es>
2020-03-01 19:20:15 +0000

    EC2 instance and VPC

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # aws_associate_architect
-Some terraform examples
+
+## EC2 instance and security group
+
+This script creates an EC2 instance and a Security Group
+
+* Look for the VPC and Subnet
+* Create Security Group and store in the VPC
+* Create EC2 instance and put in the subnet and VPC
--- ec2.tf
+++ ec2.tf
@@ -2,22 +2,51 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+data "aws_vpcs" "foo" {
+  filter {
+    name   = "tag:Name"
+    values = ["VPC_Jordi2"]
+  }
+}
+
+output "foo" {
+  value = "${data.aws_vpcs.foo.ids}"
+}
+
+data "aws_subnet" "selected" {
+  filter {
+    name   = "vpc-id"
+    values = [sort(data.aws_vpcs.foo.ids)[0]]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["PublicSubnetA"]
+  }
+}
+
+output "selected" {
+  value = "${data.aws_subnet.selected.id}"
+}
+
 resource "aws_security_group" "web_access_ec2" {
-  name = "ec2_web_access"
+  name   = "ec2_web_access"
+  vpc_id = sort(data.aws_vpcs.foo.ids)[0]
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "first_ec2" {
-  ami = "ami-0389b2a3c4948b1a0"
-  instance_type = "t2.micro"
+  ami                    = "ami-0389b2a3c4948b1a0"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web_access_ec2.id]
-  
+  subnet_id              = data.aws_subnet.selected.id
+
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
